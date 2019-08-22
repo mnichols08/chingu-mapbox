03c2c7dd5e2099088dff584b2a0ef2fe145e6b3c
Mike Nichols <mikenichols410@gmail.com>
2019-08-22 18:21:55 -0400

    added points to map

--- src/js/index.js
+++ src/js/index.js
@@ -9,20 +9,33 @@ const bounds = [
 
 const map = new mapboxgl.Map({
 container: 'map', // container id
-style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+style: 'mapbox://styles/mapbox/outdoors-v11', // stylesheet location
 center: [-79.312, 39.505], // starting position [lng, lat]
 zoom: 12, // starting zoom
 maxBounds: bounds
 });
+ 
+const geojson = require('./geofeatures/geo.json')
 
-const layerList = document.getElementById('menu');
-const inputs = layerList.getElementsByTagName('input');
- 
-function switchLayer(layer) {
-var layerId = layer.target.id;
-map.setStyle('mapbox://styles/mapbox/' + layerId);
-}
- 
-for (var i = 0; i < inputs.length; i++) {
-inputs[i].onclick = switchLayer;
-}
+
+  // add markers to map
+geojson.features.forEach(function(marker) {
+
+    // create a HTML element for each feature
+    const el = document.createElement('div');
+    el.className = 'marker';
+  
+    // make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .addTo(map);
+
+      new mapboxgl.Marker(el)
+      .setLngLat(marker.geometry.coordinates)
+      .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+        .setHTML('<h3>' + marker.properties['Park Name'] + '</h3><p>' + marker.properties.Description + '</p> <p> <strong> Activities: </strong>' + marker.properties.Activities + '</p>' ))
+      .addTo(map);
+  });
+
+
+
