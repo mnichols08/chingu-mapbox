7b7e655b7f4ac170bd38827aaf897225f4e5c9e2
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 19:09:02 -0400

    configures html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -11,6 +12,7 @@ module.exports = {
         path: path.resolve(__dirname, './public'),
         filename: 'app.js',
     },
+    plugins: [new HtmlWebpackPlugin()],
     devServer: {
         contentBase: path.resolve(__dirname, './public'),
         hot: true
