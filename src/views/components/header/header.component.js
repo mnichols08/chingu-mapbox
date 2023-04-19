9b0af5105729ff0f07a7999c4ba749d1ec96741a
Mikey Nichols <mnix@journeytocode.io>
2023-04-19 00:22:34 -0400

    imports nav component

--- src/views/components/header/header.component.js
+++ src/views/components/header/header.component.js
@@ -1,10 +1,12 @@
 import createEle from '../../../utils/createEle.util';
 import './header.style.scss';
+import renderNav from './nav/nav.component';
 
 const renderHeader = () => {
     createEle('header',`
     <h1>Garrett County Adventures</h1>
     `,document.body)
+    renderNav()
 }
 
 export default renderHeader;
