7b45be4e98e7c3d90eddce2a3a96b820e5d18a3d
Mikey Nichols <mnix@journeytocode.io>
2023-04-19 00:32:21 -0400

    removes nav from header component

--- src/views/components/header/header.component.js
+++ src/views/components/header/header.component.js
@@ -1,12 +1,10 @@
 import createEle from '../../../utils/createEle.util';
 import './header.style.scss';
-import renderNav from './nav/nav.component';
 
 const renderHeader = () => {
     createEle('header',`
     <h1>Garrett County Adventures</h1>
     `,document.body)
-    renderNav()
 }
 
 export default renderHeader;
