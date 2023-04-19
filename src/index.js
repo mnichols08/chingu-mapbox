c0563e650479a9f63efaf7c8cf61b16e359f01c8
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 21:14:40 -0400

    imports the init function and calls when body loads

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-import mapboxgl from "mapbox-gl";
-import "./styles/index.scss";
-import "mapbox-gl/dist/mapbox-gl.css";
-const main = document.querySelector("main");
-const mapBox = document.createElement("div");
-mapBox.id = "map";
-main.append(mapBox);
+import init from './views/init';
 
-mapboxgl.accessToken =
-  "pk.eyJ1IjoibW5pY2hvbHMwOCIsImEiOiJjanptM2Z4YWkwNWZzM2JtdGxzemxmOG1wIn0.NpyABUQBYrXLFQE5_0VLuQ";
-
-const map = new mapboxgl.Map({
-  container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
-});
+document.body.onload = init;
