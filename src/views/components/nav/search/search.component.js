8c6868e75bc9256eddec10a9710b4b4d32e6d01f
Mikey Nichols <mnix@journeytocode.io>
2023-04-19 14:50:17 -0400

    adds event listener to render locations on input chage

--- src/views/components/nav/search/search.component.js
+++ src/views/components/nav/search/search.component.js
@@ -1,9 +1,14 @@
 import createEle from "../../../../utils/createEle.util";
+import renderLocations from "../locations/renderLocations.component";
 import "./search.style.scss";
 
-const renderSearch = (anchor) => {
+const renderSearch = (anchor, map) => {
   const input = createEle("input", ``, anchor);
+  const ul = createEle("ul", "", anchor);
   input.placeholder = "search";
+  input.onkeyup = (e) => renderLocations(ul, map, e.target.value);
+  renderLocations(ul, map)
+  
   return input;
 };
 
