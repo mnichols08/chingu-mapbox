784af07e9861a071a9075eb63d63000f66707688
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 22:32:17 -0400

    imports mapbox css

--- src/views/components/map.component.js
+++ src/views/components/map.component.js
@@ -1,6 +1,7 @@
 import { generateMap } from "../../controller";
 import { anchor } from "../../utils/clear.util";
 import createEle from "../../utils/createEle.util";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './map.style.scss';
 
 const renderMap = (id) => {
