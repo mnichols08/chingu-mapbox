f4fe0b702e23f8f7f3ebacdf3b60e04fb9fa45f5
Mikey Nichols <mnix@journeytocode.io>
2023-04-19 01:25:02 -0400

    calls renderSearch within nav component

--- src/views/components/nav/nav.component.js
+++ src/views/components/nav/nav.component.js
@@ -1,17 +1,18 @@
 import { fetchJson } from "../../../controller";
 import createEle from "../../../utils/createEle.util";
+import renderSearch from './search/search.component';
+import './nav.style.scss';
+
 const renderNav = () => {
   const navItems = fetchJson();
-  console.log(navItems);
   const nav = createEle(
     "nav",
     `
     `,
-    document.body,
-    "",
-    "side-bar"
+    document.body
   );
   createEle("h2", "Nearby Parks", nav);
+  renderSearch(nav);
   const ul = createEle("ul", "", nav);
   navItems.features.map((item) => createEle("li", item.properties["Park Name"], ul));
   return nav;
