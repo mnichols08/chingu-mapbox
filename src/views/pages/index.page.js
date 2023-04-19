7d5cd6e3d2ca7b346b6447fc1c9a5e28590dbd28
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 23:36:12 -0400

    renders header and footer on inedex page

--- src/views/pages/index.page.js
+++ src/views/pages/index.page.js
@@ -1,7 +1,12 @@
+import renderHeader from '../components/header/header.component';
 import renderMap from "../components/map/map.component";
+import renderFooter from '../components/footer/footer.component';
+import './index.styles.scss';
 
 const indexPage = () => {
+    renderHeader();
     renderMap('map');
+    renderFooter();
 };
 
 export default indexPage;
