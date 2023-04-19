1d3d17553a5177de8478dbfe08a19a28ec8a5154
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 21:12:44 -0400

    creates a method on our controller to generate a map

---
+++ src/controller.js
@@ -0,0 +1,19 @@
+import mapboxgl from "mapbox-gl";
+export const generateMap = (id) => {
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoibW5pY2hvbHMwOCIsImEiOiJjanptM2Z4YWkwNWZzM2JtdGxzemxmOG1wIn0.NpyABUQBYrXLFQE5_0VLuQ";
+  const map = new mapboxgl.Map({
+    container: id, // container ID
+    style: "mapbox://styles/mapbox/streets-v12", // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9, // starting zoom
+  });
+  return map;
+};
+const controller = () => {
+  return {
+    renderMap,
+  };
+};
+
+export default controller;
