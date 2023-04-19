825069b708fbceb07ea55a29664431993ac727f7
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 23:38:26 -0400

    removes logic for handling smaller steps from component

--- src/views/components/map/map.component.js
+++ src/views/components/map/map.component.js
@@ -1,12 +1,17 @@
 import { generateMap } from "../../../controller";
 import { anchor } from "../../../utils/clear.util";
 import createEle from "../../../utils/createEle.util";
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './map.style.scss';
+import renderParks from './parks/park.component';
+import renderPopups from './popup/popup.component';
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./map.style.scss";
 
 const renderMap = (id) => {
   createEle("div", "", anchor, id);
-  return generateMap(id, true);
+  const map = generateMap(id, true);
+  renderParks(map);
+  renderPopups(map);
+  return map;
 };
 
 export default renderMap;
