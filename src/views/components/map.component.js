3995474a721eda5fbc8c862333550c09e6dd7660
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 21:20:59 -0400

    places style directly aside component

--- src/views/components/map.component.js
+++ src/views/components/map.component.js
@@ -1,6 +1,7 @@
 import { generateMap } from "../../controller";
 import { anchor } from "../../utils/clear.util";
 import createEle from "../../utils/createEle.util";
+import './map.style.scss';
 
 const renderMap = (id) => {
   createEle("div", "", anchor, id);
