b6f8f17b6ae7f55e4330340339c2df3534f90164
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 21:13:17 -0400

    creates a component to render a map

---
+++ src/views/components/map.component.js
@@ -0,0 +1,10 @@
+import { generateMap } from "../../controller";
+import { anchor } from "../../utils/clear.util";
+import createEle from "../../utils/createEle.util";
+
+const renderMap = (id) => {
+  createEle("div", "", anchor, id);
+  return generateMap();
+};
+
+export default renderMap;
