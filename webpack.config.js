aff76977db4610241361bd7194412788e20219fc
Mikey Nichols <mnix@journeytocode.io>
2023-04-18 20:30:24 -0400

    configures css loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
              use: ['babel-loader']
           },
           {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+          {
              test: /\.(s(a|c)ss)$/,
              use: ['style-loader','css-loader', 'sass-loader']
           }
