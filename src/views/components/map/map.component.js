f1be2eb9fe6d20b32f62adb63044cab5a5060809
Mikey Nichols <mnix@journeytocode.io>
2023-04-20 00:26:42 -0400

    removes renderParks from map

--- src/views/components/map/map.component.js
+++ src/views/components/map/map.component.js
@@ -1,7 +1,6 @@
 import { generateMap } from "../../../controller";
 import { anchor } from "../../../utils/clear.util";
 import createEle from "../../../utils/createEle.util";
-import renderParks from './parks/park.component';
 import renderPopups from './popup/popup.component';
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./map.style.scss";
@@ -9,7 +8,6 @@ import "./map.style.scss";
 const renderMap = (id) => {
   createEle("div", "", anchor, id);
   const map = generateMap(id, true);
-  renderParks(map);
   renderPopups(map);
   return map;
 };
