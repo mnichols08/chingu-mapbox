d5045a436e3c5018554408bfe944757aa62384e9
Mikey Nichols <mnix@journeytocode.io>
2023-04-20 00:29:58 -0400

    creates logic to create points on the map and move to them on click

--- src/views/components/map/parks/park.component.js
+++ src/views/components/map/parks/park.component.js
@@ -1,41 +1,19 @@
 import { fetchJson } from "../../../../controller";
-
-const renderParks = (map) => {
-  const geojson = fetchJson();
-  map.on("load", () => {
-    // Add an image to use as a custom marker
-    map.loadImage(
-      "https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png",
-      (error, image) => {
-        if (error) throw error;
-        map.addImage("custom-marker", image);
-        // Add a GeoJSON source with all points from the geojson file
-        map.addSource("points", {
-          type: "geojson",
-          data: {
-            type: "FeatureCollection",
-            features: geojson.features,
-          },
-        });
-
-        // Add a symbol layer
-        map.addLayer({
-          id: "parks",
-          type: "symbol",
-          source: "points",
-          layout: {
-            "icon-image": "custom-marker",
-            // get the title name from the source's "Park Name" property
-            "text-field": ["get", "Park Name"],
-            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-            "text-offset": [0, 1.25],
-            "text-anchor": "top",
-          },
-        });
-      }
-    );
-  });
-  return map;
+import { flyToStore, openPopup } from "../../../../utils/mapbox.util";
+import mapboxgl from "mapbox-gl";
+import "./park.style.scss";
+const renderParks = (map, feature) => {
+  // create a HTML element for each feature
+  const el = document.createElement("div");
+  el.className = "marker";
+  el.id = `marker-${feature.properties.id}`;
+  new mapboxgl.Marker(el)
+    .setLngLat(feature.geometry.coordinates.slice())
+    .addTo(map);
+  el.onclick = function () {
+    document.getElementById(`link-${feature.properties.id}`).click();
+    openPopup(map, feature);
+  };
 };
 
 export default renderParks;
